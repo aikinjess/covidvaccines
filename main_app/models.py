844ffa4c955e63c8f14cea36749057e63fd281b3
aikinjess <aikinjess@gmail.com>
2021-05-15 17:59:41 -0400

    views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+  name = models.CharField(max_length=100)
+  dob = models.CharField(max_length=100)
+  phoneNo = models.TextField(max_length=250)
+  address = models.TextField(max_length=250)
+  cityState = models.TextField(max_length=250)
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Patient
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def patients_index(request):
+  patients = Patient.objects.all()
   return render(request, 'patients/index.html', { 'patients': patients })
 
 class Patient:  # Note that parens are optional if not inheriting from another class
