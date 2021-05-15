5d2943d40ecb292a0f26c1373286f9cc152697e3
aikinjess <aikinjess@gmail.com>
2021-05-15 19:23:12 -0400

    create view

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   # route for cats index
   path('patients/', views.patients_index, name='index'),
   path('patients/<int:patient_id>/', views.patients_detail, name='detail'),
+  path('patients/create/', views.PatientCreate.as_view(), name='patients_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Patient
 # Create your views here.
 
@@ -16,3 +17,6 @@ def patients_detail(request, patient_id):
   patient = Patient.objects.get(id=patient_id)
   return render(request, 'patients/detail.html', { 'patient': patient })
 
+class PatientCreate(CreateView):
+  model = Patient
+  fields = '__all__'
